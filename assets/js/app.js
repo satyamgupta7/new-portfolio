76b65109e2a3dc1f3b563a10070363d10cf014d7
Satyam <satyamgupta.er@gmail.com>
2021-10-11 17:08:55 +0530

    added email configuration

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,33 @@ scrollUp.addEventListener("click", () => {
 //     ul.classList.remove("show");
 //   })
 // );
+
+function sendmail() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  if (name == "" || email == "" || message == "") {
+    alert("Please enter valid input");
+    return;
+  }
+
+  var Body =
+    "Name: " + name + "<br>Email: " + email + "<br>Message: " + message;
+  //console.log(name, phone, email, message);
+  Email.send({
+    SecureToken: "fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
+    To: "satyamgupta.er@gmail.com",
+    From: "satyamgupta.er@gmail.com",
+    Subject: "New message on contact from " + name,
+    Body: Body,
+  }).then((message) => {
+    //console.log (message);
+    if (message == "OK") {
+      alert("Congratulations..!! We have received your enquiry request.");
+    } else {
+      console.error(message);
+      alert("There is error at sending message. ");
+    }
+  });
+}
